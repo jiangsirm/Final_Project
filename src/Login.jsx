161073da6eb8581a6953e0053e513707b3acbde3
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-04-18 11:42:24 -0700

    updates

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 function Login() {
     return(
         <div>
-            This is the Login Page. Alex will change it later
+            This is the Login Page. Alex will change it later. Josh Here. 
         </div>
     )
 }
