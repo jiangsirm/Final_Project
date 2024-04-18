b6068a7d6310282ce6fe26c75c4960a017aeb6ef
jiangsirm <jycsirm@gmail.com>
2024-04-18 14:13:37 -0700

    changeServerforDeploy

--- server.cjs
+++ server.cjs
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const password = require('./backend/password.api.cjs')
 const account = require('./backend/account.api.cjs')
+const path = require('path')
 
 // const path = require('path');
 
@@ -33,14 +34,22 @@ app.use('/api/account', account);
 //     res.sendFile('index.html', { root: rootDir });
 // });
 
-app.get('/', function(request, response){
-    response.send("Paqiuli Go!")
-});
+// app.get('/', function(request, response){
+//     response.send("Paqiuli Go!")
+// });
 
-app.put("/", function(request, response) {
-    response.send("Put on your boots!")
-    }
-)
+// app.put("/", function(request, response) {
+//     response.send("Put on your boots!")
+//     }
+// )
+
+let frontend_dir = path.join(__dirname, 'dist')
+
+app.use(express.static(frontend_dir));
+app.get('*', function (req, res) {
+    console.log("received request");
+    res.sendFile(path.join(frontend_dir, "index.html"));
+});
 
 app.listen(process.env.PORT || 8000, function(){
     console.log("deep dark fantasy");
