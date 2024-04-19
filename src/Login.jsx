d1241f5c5e85d0d891048af7938f757d68f2ea2b
JoshuaFancher25 <137542280+JoshuaFancher25@users.noreply.github.com>
2024-04-19 11:07:52 -0700

    added verbiage to login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,21 @@
 function Login() {
     return(
         <div>
-            This is the Login Page. Alex will change it later. Josh Here. 
+        <div>
+            <h3>Password Manager App.</h3>
+            <p>Product Description:</p>
+            <p>The Password Manager App serves as a secure repository for storing login credentials. 
+                Users can manage their usernames and passwords efficiently, 
+                choosing to either enter their own passwords or generate new ones 
+                with customizable complexity. Additionally, the app provides a feature to 
+                securely share passwords with other users as needed.</p>
+        </div>
+        <br/>
+        <div>
+            Creators: Yuchen Jiang, Kun Cao, Josh Fancher
+        </div>
+        <br/>
+       
         </div>
     )
 }
